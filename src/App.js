d5ccac8da35b5057773d49853ca6c1904210c257
Divya Tiwari <divya@carta.healthcare>
2021-07-04 23:31:03 +0530

    Blinking digital clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { currentTime: '' }
+    
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      let currDateTime = new Date().toLocaleString();
+      let separatedDateTime = currDateTime.split(",");
+      let currTime = separatedDateTime[1];
+
+      this.setState({ currentTime: currTime });
+      this.getRandomColor()
+
+    }, 1000)
+  }
+
+  getRandomColor = () => {
+    let color_code = "";
+    let used_colors = [];
+    while ((color_code.length !== 7) || (color_code.length === 7 && used_colors.includes(color_code))) {
+      color_code = Math.floor(Math.random() * 16777215).toString(16);
+      color_code = "#" + color_code;
+    }
+
+    used_colors.push(color_code);
+
+    return color_code;
+  }
+
+  
+  render() {
+
+    const { currentTime } = this.state;
+    const splitTime = currentTime.split(":");
+
+    return (
+      <div className="clock">
+        {splitTime.map((time, i) => {
+          console.log(splitTime.length, i);
+          return (i!==splitTime.length-1) ? (
+            <p
+              key={i} 
+              style={{color: this.getRandomColor()}}
+              >{` ${time}:`}
+            </p>)
+            : (
+            <p
+            key={i} 
+            style={{color: this.getRandomColor()}}
+            >{` ${time} `}
+            </p>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
