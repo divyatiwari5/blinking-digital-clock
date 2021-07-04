a24a2471aaf3fed2112abf8aaf9297262532a627
Divya Tiwari <divya@carta.healthcare>
2021-07-05 00:35:47 +0530

    Setup enzyme, jest; writing test case for getRandomColor()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { configure, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+let root;
+
+beforeAll(() => {
+  root = mount(<App/>);
+});
+
+afterAll(() => {
+  root = null;
+});
+
+describe('Check random color generation', () => {
+  it('checks if random color is being generated', () => {
+    let color_code='';
+    let used_colors = [];
+  
+    while((color_code !== 7) || (color_code === 7 && used_colors.includes(color_code))) {
+      color_code = '#bf2199';
+      expect(root.instance().getRandomColor()).toBe('#bf2199');
+    }
+  
+    expect(root.instance().used_colors).toBe(['#bf2199']);
+    expect(root.instance().color_code).toBe('#bf2199');
+  });
+  
 });
